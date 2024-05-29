2e61dfab11e660aac68ffd286a14af8a569eaa56
Грудинин Алексей Михайлович <ip-man2013@yandex.ru>
2024-05-29 22:20:53 +0300

    Замена Date на LocalDateTime + рефакторинг

--- src/main/java/ru/learning/searchengine/persistance/repositories/impl/HealthCheckRepositoryImpl.java
+++ src/main/java/ru/learning/searchengine/persistance/repositories/impl/HealthCheckRepositoryImpl.java
@@ -1,16 +1,14 @@
 package ru.learning.searchengine.persistance.repositories.impl;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 import ru.learning.searchengine.persistance.repositories.HealthCheckRepository;
 
 @Repository
+@RequiredArgsConstructor
 public class HealthCheckRepositoryImpl implements HealthCheckRepository {
     private final JdbcTemplate jdbcTemplate;
-
-    public HealthCheckRepositoryImpl(JdbcTemplate jdbcTemplate) {
-        this.jdbcTemplate = jdbcTemplate;
-    }
 
     public void performDbHealthCheck() {
         jdbcTemplate.queryForObject("SELECT 1", Integer.class); // Выполнение нативного SQL-запроса
